6a06a09ea476028cd98d5fe73c59e0119d671752
yaararm <yaararm@gmail.com>
2020-05-29 13:26:32 +0300

    all

---
+++ src/main/java/Client/AlertsApp.java
@@ -0,0 +1,25 @@
+package Client;
+
+import PresentationLayer.GuiApp;
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.util.Collections;
+
+
+@SpringBootApplication
+public class AlertsApp extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+
+
+        Application.launch(GuiApp.class,args);
+
+
+
+    }
+
+
+}
